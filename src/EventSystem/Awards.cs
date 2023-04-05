3fa1d55d652834f0a9d6513405caeb64a1a49efa
Awbugl <me@awbugl.top>
2023-04-05 20:04:15 +0800

    Update AddVeinAwards

--- src/EventSystem/Awards.cs
+++ src/EventSystem/Awards.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace DSP_EventSystem
 {
@@ -24,16 +25,62 @@ namespace DSP_EventSystem
 
         // TODO: Implement all awards
 
-        private static void AddItemAwards(int[] value)
+        public static void AddItemAwards(int[] value)
         {
             for (var i = 0; i < value.Length; i += 2) GameMain.history.GainTechAwards(value[i], value[i + 1]);
         }
 
-        private static void AddVeinAwards(int[] value) => throw new NotImplementedException();
+        public static void AddVeinAwards(int[] value)
+        {
+            for (var i = 0; i < value.Length; i += 2)
+            {
+                var pos = GameMain.mainPlayer.position;
 
-        private static void AddEntityAwards(int[] value) => throw new NotImplementedException();
+                var factory = GameMain.mainPlayer.planetData.factory;
+                var planet = factory.planet;
 
-        private static void AddTechHashAwards(int[] value)
+                var veinProto = LDB.veins.Select(value[i]);
+                if (veinProto == null) return;
+                var veinData = new VeinData { type = (EVeinType)veinProto.ID, modelIndex = (short)veinProto.ModelIndex, amount = value[i + 1] };
+
+                if (veinData.amount < 1) veinData.amount = 1;
+                if (veinData.amount > 1000000000) veinData.amount = 1000000000;
+                veinData.productId = veinProto.MiningItem;
+
+                veinData.pos = pos;
+                if (veinData.type == EVeinType.Oil)
+                {
+                    veinData.pos = planet.aux.RawSnap(veinData.pos);
+                    veinData.pos = veinData.pos.normalized * planet.data.QueryModifiedHeight(pos);
+                }
+
+                veinData.minerCount = 0;
+                factory.AssignGroupIndexForNewVein(ref veinData);
+                var index1 = factory.AddVeinData(veinData);
+                VeinData[] veinPool = factory.veinPool;
+                veinPool[index1].modelId
+                    = planet.factoryModel.gpuiManager.AddModel(veinPool[index1].modelIndex, index1, veinPool[index1].pos,
+                                                               Maths.SphericalRotation(pos, 0.0f));
+                ColliderData[] colliders = veinProto.prefabDesc.colliders;
+                
+                for (var index2 = 0; colliders != null && index2 < colliders.Length; ++index2)
+                {
+                    veinPool[index1].colliderId
+                        = planet.physics.AddColliderData(colliders[index2].BindToObject(index1, veinPool[index1].colliderId, EObjectType.Vein,
+                                                                                        veinPool[index1].pos,
+                                                                                        Quaternion.FromToRotation(Vector3.up,
+                                                                                                                  veinPool[index1].pos.normalized)));
+                }
+
+                factory.RefreshVeinMiningDisplay(index1, 0, 0);
+
+                factory.RecalculateVeinGroup(factory.veinPool[index1].groupIndex);
+            }
+        }
+
+        public static void AddEntityAwards(int[] value) => throw new NotImplementedException();
+
+        public static void AddTechHashAwards(int[] value)
         {
             for (var i = 0; i < value.Length; i += 2)
             {
@@ -42,23 +89,23 @@ namespace DSP_EventSystem
                 if (techState.unlocked) continue;
 
                 techState.hashUploaded += Math.Min(techState.hashNeeded - techState.hashUploaded, value[i + 1] * techState.hashNeeded / 100);
-                
+
                 GameMain.history.techStates[value[i]] = techState;
             }
         }
 
-        private static void TriggerItemEventAwards(int[] value) => throw new NotImplementedException();
+        public static void TriggerItemEventAwards(int[] value) => throw new NotImplementedException();
 
-        private static void TriggerVeinEventAwards(int[] value) => throw new NotImplementedException();
+        public static void TriggerVeinEventAwards(int[] value) => throw new NotImplementedException();
 
-        private static void TriggerEntityEventAwards(int[] value) => throw new NotImplementedException();
+        public static void TriggerEntityEventAwards(int[] value) => throw new NotImplementedException();
 
-        private static void TriggerPlanetEventAwards(int[] value) => throw new NotImplementedException();
+        public static void TriggerPlanetEventAwards(int[] value) => throw new NotImplementedException();
 
-        private static void TriggerStarEventAwards(int[] value) => throw new NotImplementedException();
+        public static void TriggerStarEventAwards(int[] value) => throw new NotImplementedException();
 
-        private static void PlanetEffectAwards(int[] value) => throw new NotImplementedException();
+        public static void PlanetEffectAwards(int[] value) => throw new NotImplementedException();
 
-        private static void StarEffectAwards(int[] value) => throw new NotImplementedException();
+        public static void StarEffectAwards(int[] value) => throw new NotImplementedException();
     }
 }
